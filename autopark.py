d4a859337ebb3dbd56429f6d70cf4dab63bd6a20
Kaludev <luka.markovic2017@gmail.com>
2022-10-21 16:03:57 +0200

    modified:   autopark.py Fixed steering

--- autopark.py
+++ autopark.py
@@ -59,9 +59,9 @@ class Player:
     def draw(self):
         rectRotated(screen,WHITE,(int(self.x),int(self.y),self.a,self.b),0,15,self.angle)
     def rotatep(self):
-        self.angle +=1.5
+        self.angle +=2.5
     def rotaten(self):
-        self.angle -=1.5
+        self.angle -=2.5
     def backward(self):
         self.x -= self.speed * float(math.sin((self.angle+90)*(math.pi/180)))
         self.y -= self.speed * float(math.cos((self.angle+90)*(math.pi/180)))
